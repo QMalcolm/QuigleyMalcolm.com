e405d0f5af4f21675969491c8b2828c642dedb17
T. Quigley Malcolm <tqmalcolm@protonmail.com>
2021-01-07 14:22:10 -0800

    Add slugger to slugify things

--- mix.exs
+++ mix.exs
@@ -47,7 +47,8 @@ defmodule QuigleyMalcolm.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:faker, "~> 0.16.0"}
+      {:faker, "~> 0.16.0"},
+      {:slugger, "~> 0.3.0"}
     ]
   end
 
